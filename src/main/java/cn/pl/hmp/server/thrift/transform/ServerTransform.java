98aa225006d8857a9f724e54453ddd1e4d25f028
Alan Yuan <yuanqi@passiontec.cn>
2015-11-14 20:24:10 +0800

    增加分页转换

--- src/main/java/cn/pl/hmp/server/thrift/transform/ServerTransform.java
+++ src/main/java/cn/pl/hmp/server/thrift/transform/ServerTransform.java
@@ -4,7 +4,9 @@
  */
 package cn.pl.hmp.server.thrift.transform;
 
+import cn.pl.commons.pages.Pages;
 import cn.pl.frame.client.thrift.transform.ThriftTransform;
+import cn.pl.frame.thrift.define.TPages;
 import cn.pl.hmp.commons.thrift.define.TExample;
 import cn.pl.hmp.server.dao.entity.HmpTest;
 
@@ -28,6 +30,56 @@ public class ServerTransform extends ThriftTransform {
         TExample result = new TExample();
         result.setId(rlt.getId());
         result.setName(rlt.getName());
+        return result;
+    }
+    
+    public static Pages transform(TPages pages){
+        if(pages == null)
+            return null;
+        Pages result = new Pages();
+        result.setPageNum(pages.getPageNum());
+        result.setPageSize(pages.getPageSize());
+        result.setSize(pages.getSize());
+        result.setOrderBy(pages.getOrderBy());
+        result.setEndRow(pages.getEndRow());
+        result.setTotal(pages.getTotal());
+        result.setPages(pages.getPages());
+        result.setFirstPage(pages.getFirstPage());
+        result.setPrePage(pages.getPrePage());
+        result.setNextPage(pages.getNextPage());
+        result.setLastPage(pages.getLastPage());
+        result.setOnFirstPage(pages.isOnFirstPage());
+        result.setOnLastPage(pages.isOnLastPage());
+        result.setHasPreviousPage(pages.isHasPreviousPage());
+        result.setHasNextPage(pages.isHasNextPage());
+        result.setNavigatePages(pages.getNavigatePages());
+        result.setNavigatepageNums(pages.getNavigatepageNums());
+        
+        return result;
+    }
+    
+    public static TPages transform(Pages pages){
+        if(pages == null)
+            return null;
+        TPages result = new TPages();
+        result.setPageNum(pages.getPageNum());
+        result.setPageSize(pages.getPageSize());
+        result.setSize(pages.getSize());
+        result.setOrderBy(pages.getOrderBy());
+        result.setEndRow(pages.getEndRow());
+        result.setTotal(pages.getTotal());
+        result.setPages(pages.getPages());
+        result.setFirstPage(pages.getFirstPage());
+        result.setPrePage(pages.getPrePage());
+        result.setNextPage(pages.getNextPage());
+        result.setLastPage(pages.getLastPage());
+        result.setOnFirstPage(pages.isOnFirstPage());
+        result.setOnLastPage(pages.isOnLastPage());
+        result.setHasPreviousPage(pages.isHasPreviousPage());
+        result.setHasNextPage(pages.isHasNextPage());
+        result.setNavigatePages(pages.getNavigatePages());
+        result.setNavigatepageNums(pages.getNavigatepageNums());
+        
         return result;
     }
 }
