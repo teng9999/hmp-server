c4e28af0bf143ed9223d1e33a17b965ada9de316
Alan Yuan <yuanqi@passiontec.cn>
2015-12-15 18:45:12 +0800

    改小server内存配置

--- src/main/bin/java_env.sh
+++ src/main/bin/java_env.sh
@@ -4,6 +4,6 @@ export JAVA_HOME=/home/work/jdk-1.8
 export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 
-java_max_heap_size=1024M
-java_initail_heap_size=1024M
-java_young_heap_size=512M
+java_max_heap_size=512M
+java_initail_heap_size=512M
+java_young_heap_size=256M
